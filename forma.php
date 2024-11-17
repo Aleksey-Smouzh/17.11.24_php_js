8854e0fdcd52f0c11a2153c60934fb2a4b857401
Aleksey-Smouzh <xmz@tut.by>
2024-11-17 12:16:51 +0100

    form js and php

---
+++ forma.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>forma</title>
+</head>
+
+<body>
+<!-- --------------------------------------------------------- -->
+    <h1>js form</h1>
+    <form onsubmit="return sprawdzForm()">
+
+        <label for="imie"></label>
+
+        <input type="text" id="imie">
+
+        <button type="submit">wyslij</button>
+    </form>
+
+    <p id="blad"></p>
+
+    <script>
+        const sprawdzForm = () => {
+            
+            const input = document.getElementById('imie').value
+
+            if (input === "") {
+                document.getElementById('blad').innerHTML = "nic ne ma"
+                return false
+            } 
+            return true
+        }
+    </script>
+<!-- ---------------------------------------------------------------------- -->
+
+<h2>php form</h2>
+<form method="POST">
+        <label for="imie"></label>
+
+        <input type="text" id="imie" name="imie">
+
+        <button type="submit">wyslij</button>
+    </form>
+
+<?php
+
+if($_SERVER["REQUEST_METHOD"]== "POST"){
+    $imie = $_POST["imie"];
+    if(empty($imie)){
+echo"<p>tu ne mozna byc puste</p>";
+    }else{
+        echo"<p> hi, $imie !</p>";
+    }
+}
+
+
+
+
+
+
+
+
+
+?>
+
+
+
+
+
+
+
+
+</body>
+
+</html>
